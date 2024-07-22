4fbb90d52ca24ea4cbb105a9265cd8c247f01a1a
Vivek Antony <Vivek.Antony@philips.com>
2024-07-22 17:42:50 +0530

    sensor statistics changes

--- stats.h
+++ stats.h
@@ -1,7 +1,16 @@
+#include <numeric>
 #include <vector>
+#include <algorithm>
+#include <math.h>
 
 namespace Statistics {
     // define the Stats structure here. See the tests to infer its properties
-    
-    Stats ComputeStatistics(const std::vector<___>& );
+    struct Stats
+    {
+        double average;
+        double min;
+        double max;
+    };
+
+    Stats ComputeStatistics(const std::vector<double>& );
 }
